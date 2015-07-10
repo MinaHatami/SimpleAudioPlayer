e01b31f3f7a84fa20f1d19fd813c02b2b48159ca
mina <mina@MINAPC>
2015-07-10 13:06:14 -0400

    Add stop functionality.

--- src/com/minahatami/audiomanager/MainActivity.java
+++ src/com/minahatami/audiomanager/MainActivity.java
@@ -1,11 +1,15 @@
 package com.minahatami.audiomanager;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -14,16 +18,20 @@ public class MainActivity extends Activity {
 	int volValue = 6;
 	AudioManager audioManager;
 	MediaPlayer myPlayer;
+	Button btPlay, btStop;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		btPlay = (Button) findViewById(R.id.btPlay);
+		btStop = (Button) findViewById(R.id.btStop);
+
 		tvVolume = (TextView) findViewById(R.id.textView1);
 		tvVolume.setText("" + volValue);
 
-		myPlayer = MediaPlayer.create(this, R.raw.slow_whoop_bubble_pop);
+		myPlayer = MediaPlayer.create(this, R.raw.shekeitoff);
 
 	}
 
@@ -32,10 +40,8 @@ public class MainActivity extends Activity {
 			volValue -= 2;
 		}
 		tvVolume.setText("" + volValue);
-		
-		float volume = (float)volValue/10 ;
-		Log.v("Volume", "Volume is: " + volume);
-		myPlayer.setVolume(volume, volume);
+		setVolumeLevel(volValue);
+
 	}
 
 	public void upVolumeClick(View view) {
@@ -43,14 +49,32 @@ public class MainActivity extends Activity {
 			volValue += 2;
 		}
 		tvVolume.setText("" + volValue);
+		setVolumeLevel(volValue);
+	}
+
+	public void playClick(View view) throws IllegalStateException, IOException {
+		if (!myPlayer.isPlaying() && btPlay.isEnabled()) {
+			
+			myPlayer.start();
+			btPlay.setEnabled(false);
+			btStop.setEnabled(true);
+		}
 		
-		float volume = (float)volValue/10 ;
+	}
+
+	public void stopClick(View view) throws IllegalStateException, IOException {
+		if (myPlayer.isPlaying() && btStop.isEnabled()) {
+			myPlayer.stop();
+			btStop.setEnabled(false);
+			btPlay.setEnabled(true);
+		}
+	}
+	
+	
+
+	public void setVolumeLevel(int volValue) {
+		float volume = (float) volValue / 10;
 		Log.v("Volume", "Volume is: " + volume);
 		myPlayer.setVolume(volume, volume);
 	}
-
-	public void playClick(View view) {
-		myPlayer.start();
-	}
-
 }
